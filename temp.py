e6cf42261e369ad763123ae13150d4ca6c2040d0
mollyya <144211230+mollyya@users.noreply.github.com>
2023-09-09 11:19:48 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+hight=float(input("請輸入您的身高(公尺):"))
+weight=float(input("請輸入您的體重(公斤):"))
+BMI=weight/(hight**2)
+print(f"您的BMI是:{BMI:.2f}")
